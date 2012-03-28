2352c999cde9ce3074686f003ab809ca38e40242
Murilo <91murilo@gmail.com>
2012-03-28 18:31:01 -0300

    First Commit! Projeto criado!

---
+++ YambaUnicamp/src/br/unicamp/yamba/YambaUnicampActivity.java
@@ -0,0 +1,13 @@
+package br.unicamp.yamba;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class YambaUnicampActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
